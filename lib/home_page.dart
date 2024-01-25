963182b7c4009784e3bf22430ece660a32129295
Aditya Dwivedi <dwikaljit@gmail.com>
2024-01-25 13:31:06 +0530

    finalised the boilerplate UI.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,66 +10,75 @@ class HomePage extends StatelessWidget {
         title: const Text('Color Slider'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    height: 200,
-                    width: 180,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(20),
-                      border: const Border(
-                        top: BorderSide(
-                          color: Colors.black,
-                          width: 3,
-                        ),
-                        left: BorderSide(
-                          color: Colors.black,
-                          width: 3,
-                        ),
-                        bottom: BorderSide(
-                          color: Colors.black,
-                          width: 3,
-                        ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 200,
+                  width: 180,
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                    ),
+                    border: Border(
+                      top: BorderSide(
+                        color: Colors.white,
+                        width: 10,
+                      ),
+                      left: BorderSide(
+                        color: Colors.white,
+                        width: 10,
+                      ),
+                      bottom: BorderSide(
+                        color: Colors.white,
+                        width: 10,
                       ),
                     ),
                   ),
-                  Container(
-                    height: 200,
-                    width: 180,
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(20),
-                      border: const Border(
-                        top: BorderSide(
-                          color: Colors.black,
-                          width: 3,
-                        ),
-                        left: BorderSide(
-                          color: Colors.black,
-                          width: 3,
-                        ),
-                        bottom: BorderSide(
-                          color: Colors.black,
-                          width: 3,
-                        ),
+                ),
+                Container(
+                  height: 200,
+                  width: 180,
+                  decoration: const BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
+                    border: Border(
+                      top: BorderSide(
+                        color: Colors.white,
+                        width: 10,
+                      ),
+                      right: BorderSide(
+                        color: Colors.white,
+                        width: 10,
+                      ),
+                      bottom: BorderSide(
+                        color: Colors.white,
+                        width: 10,
                       ),
                     ),
                   ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Slider(
-                value: 0.2,
-                onChanged: (value) {},
-              )
-            ],
-          ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Slider(
+              min: 0,
+              max: 1,
+              value: 0.2,
+              onChanged: (value) {},
+            )
+          ],
         ),
       ),
     );
